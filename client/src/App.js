ecf416581022f2f1d61717045534bcecc9e34a91
Shing Hei Chan (Tommy) <shingchan1219@gmail.com>
2024-10-25 17:43:48 +0100

    update layout with CSS styling
    Added CSS classes for spacing, flex layout, and responsive styling to each section of the layout

--- client/src/App.js
+++ client/src/App.js
@@ -7,14 +7,13 @@ import Pagination from './components/Pagination';
 
 function App() {
   return (
-    <div>
+    <div className='min-h-screen flex flex-col bg-gray-100'>
       <Header />
-      <div>
-        <aside>
+      <div className='flex flex-1 p-4'>
+        <aside className='w-1/4 p-4 bg-white shadow-md'>
           <FilterSection />
         </aside>
-
-        <section>
+        <section className='flex-1 ml-4'>
           <SortSection />
           <ProductCard />
           <Pagination />
