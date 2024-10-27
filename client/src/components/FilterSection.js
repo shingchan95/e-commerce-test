6acb4444ce42cf9291d5977029d7e35b58ded335
Shing Hei Chan (Tommy) <shingchan1219@gmail.com>
2024-10-27 19:14:53 +0000

    Update filtersection
    updated filter dropdown using div with Daisy UI, also nested facets and categories option under filter

--- client/src/components/FilterSection.js
+++ client/src/components/FilterSection.js
@@ -10,43 +10,44 @@ function FilterSection({ setQuery, facets }) {
     };
     return (
         <div>
-            <h3 className="font-bold mb-4">Filter</h3>
-            <div className="mb-4">
-                {/* collapsible categories section */}
-                <details className="collapse mb-2">
-                    <summary className="bg-base-200 collapse-title px-6">
-                        <div className='flex justify-between'>
+            <div className="collapse collapse-arrow">
+                <input type="checkbox" className="peer" />
+                <div className="bg-base-200 collapse-title font-semibold text-left cursor-pointer">
+                    <h3 className="font-bold">Filter</h3>
+                </div>
+
+                <div className="collapse-content">
+
+                    {/* collapsible categories section */}
+                    <div className="collapse my-2 collapse-arrow">
+                        <input type="checkbox" className="peer" />
+                        <div className="bg-base-200 collapse-title px-6 flex justify-between">
                             <span>Categories</span>
-                            <span className="self-end">&#x2b;</span>
                         </div>
-                    </summary>
-                    <div className="collapse-content px-4 py-2 ">
-                        {categories.map((category) => (
-                            <button
-                                key={category}
-                                onClick={() => handleCategoryClick(category)}
-                                className={`w-full p-2 text-left border rounded-lg hover:bg-gray-100 ${selectedCategory === category ? 'bg-gray-300' : ''}`}
-                            >
-                                {category.charAt(0).toUpperCase() + category.slice(1)}
-                            </button>
-                        ))}
+                        <div className="collapse-content">
+                            {categories.map((category) => (
+                                <button
+                                    key={category}
+                                    onClick={() => handleCategoryClick(category)}
+                                    className={`w-full p-2 my-2 text-left border rounded-lg hover:bg-gray-100 ${selectedCategory === category ? 'bg-gray-300' : ''}`}
+                                >
+                                    {category.charAt(0).toUpperCase() + category.slice(1)}
+                                </button>
+                            ))}
+                        </div>
                     </div>
-                </details>
 
 
-                {/*  the component is missing the logic to apply facet selections to the API call.
+                    {/*  the component is missing the logic to apply facet selections to the API call.
                 facet handling with the API is yet to be implemented. */}
 
-                {/* other facets section */}
-                {facets.map((facet) => (
-                    <div key={facet.identifier} className="mb-2">
-                        <details className="collapse">
-                            <summary className="bg-base-200 collapse-title px-6">
-                                <div className='flex justify-between'>
-                                    <span>{facet.displayName}</span>
-                                    <span className="self-end">&#x2b;</span>
-                                </div>
-                            </summary>
+                    {/* other facets section */}
+                    {facets.map((facet) => (
+                        <div key={facet.identifier} className="collapse collapse-arrow mb-2">
+                            <input type="checkbox" className="peer" />
+                            <div className='bg-base-200 collapse-title px-6 flex justify-between'>
+                                <span>{facet.displayName}</span>
+                            </div>
                             <div className='collapse-content'>
                                 {facet.options.map((option) => (
                                     <div key={option.identifier} className="flex items-center mb-2">
@@ -62,12 +63,13 @@ function FilterSection({ setQuery, facets }) {
                                     </div>
                                 ))}
                             </div>
-                        </details>
-                    </div>
-                ))}
+                        </div>
+                    ))}
+                </div>
             </div>
-        </div>
+        </div >
     );
 }
 
 export default FilterSection;
+
